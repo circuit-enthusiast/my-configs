64983de0322eea260e7d57750c4203dac41d329a
AF <gab.fat8@gmail.com>
2026-08-28 13:55:50 -0400

    feat: added if statement to setup script to copy astro-nvim template configs

--- scripts/setup.ps1
+++ scripts/setup.ps1
@@ -1,8 +1,9 @@
 # Moves config to their default location
 
 # Set script variables
-echo $PSScriptRoot;
+#echo $PSScriptRoot;
 $NVIM_DOTFILE = "$PSScriptRoot\..\nvim"
+$ASTRO_NVIM_TEMPLATE_DOTFILE = "$PSScriptRoot\..\astro-nvim-template-main"
 # $OMP_DOTFILE = "$PSScriptRoot\nvim-"
 $NVIM_DIR_PATH = "$env:LOCALAPPDATA\nvim"
 $NVIM_DATA_PATH = "$env:LOCALAPPDATA\nvim-data"
@@ -10,4 +11,11 @@ $NVIM_DATA_PATH = "$env:LOCALAPPDATA\nvim-data"
 
 # Nvim configs
 & "$PSScriptRoot\clean.ps1"
-cp -r $NVIM_DOTFILE $env:LOCALAPPDATA
+
+if($args[0] -eq $null -or $args[0] -eq "nvim"){
+  Write-Output "Setting up custom nvim configs in $NVIM_DIR_PATH"
+  cp -r $NVIM_DOTFILE $env:LOCALAPPDATA;
+} elseif ($args[0] -eq "astro"){
+  Write-Output "Setting up astro nvim template configs in $NVIM_DIR_PATH"
+  cp -r $ASTRO_NVIM_TEMPLATE_DOTFILE\* $NVIM_DIR_PATH;
+}
